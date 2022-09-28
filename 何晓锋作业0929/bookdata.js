4c9210d4bfff884e72412cb56f36b00b0df552a3
hexiaofeng <1252533955@qq.com>
2022-09-28 13:59:50 +0800

    初始化二阶段学习内容

---
+++ 何晓锋作业0929/bookdata.js
@@ -0,0 +1,59 @@
+ var bookArr = [{
+     img: "http://img3m4.ddimg.cn/87/2/26509614-1_b_28.jpg",
+     title: "中国孩子钟爱的十万个为什么•中国孩子钟爱的十万个为什么(套装全八册) ",
+     newprice: "36.00",
+     oldPrice: "80.00",
+     num: 1234,
+     author: "[美]卡勒德·胡赛尼　著"
+   }, {
+     img: "http://img3m2.ddimg.cn/17/4/28977812-1_b_13.jpg",
+     title: "快乐读书吧 2年级下(5册) ",
+     newprice: "41.40",
+     oldPrice: "92.00",
+     num: 345,
+     author: "冯唐"
+   },
+   {
+     img: "https://img0.winxuancdn.com/1207/1201671207_0_3_800x800.jpg?1555310022254&imageMogr2/thumbnail/160x160 ",
+     title: "林格伦作品选集•长袜子皮皮（美绘版） ",
+     newprice: "16.80",
+     oldPrice: "23.00",
+     num: 23,
+     author: "简媜"
+
+   },
+   {
+     img: "https://img3.winxuancdn.com/8151/1201288151_49.jpg?1541063897419&imageMogr2/thumbnail/160x160 ",
+     title: "曹文轩纯美小说系列•草房子 ",
+     newprice: "15.90",
+     oldPrice: "22.00",
+     num: 9753,
+     author: "张爱玲"
+   },
+   {
+     img: "https://img0.winxuancdn.com/0951/1202250951_0_0_original_800x800.jpg?1591497028920&imageMogr2/thumbnail/200x200",
+     title: "还是钟南山",
+     newprice: "43.20",
+     oldPrice: "48.00",
+     num: 64,
+     author: "酒酿圆子"
+
+   },
+   {
+     img: "https://img0.winxuancdn.com/9516/1201609516_0_3_800x800.jpg?1555319894918&imageMogr2/thumbnail/160x160 ",
+     title: "夏洛书屋:经典版 总有一天会长大 ",
+     newprice: "15.00",
+     oldPrice: "19.00",
+     num: 252433,
+     author: "庆山"
+   },
+   {
+     img: "https://img2.winxuancdn.com/6665/1202206665_0_1_800x800.jpg?1584609248880&imageMogr2/thumbnail/200x200",
+     title: "生鲜电商的新零售之路",
+     newprice: "51.80",
+     oldPrice: "69.00",
+     num: 1320,
+     author: "严歌苓"
+   }
+
+ ]
